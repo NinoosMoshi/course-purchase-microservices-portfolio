90c8117b1fcbd50cda9adfef16ce119e728027f0
NinoosMoshi <nalkaton@gmail.com>
2021-10-26 21:04:40 -0400

    create course-microservices

---
+++ backEnd/course-microservice1/src/main/java/com/ninos/coursemicroservice1/CourseMicroservice1Application.java
@@ -0,0 +1,13 @@
+package com.ninos.coursemicroservice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseMicroservice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseMicroservice1Application.class, args);
+	}
+
+}
---
+++ backEnd/course-microservice1/src/test/java/com/ninos/coursemicroservice1/CourseMicroservice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ninos.coursemicroservice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseMicroservice1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
