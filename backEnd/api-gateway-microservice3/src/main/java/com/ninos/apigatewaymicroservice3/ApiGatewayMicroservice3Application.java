f4a20fed8d113d28f941c2e058c2bbc6f980086c
NinoosMoshi <nalkaton@gmail.com>
2021-10-29 10:46:06 -0400

    Spring api-gateway microservice Closes #13

---
+++ backEnd/api-gateway-microservice3/src/main/java/com/ninos/apigatewaymicroservice3/ApiGatewayMicroservice3Application.java
@@ -0,0 +1,13 @@
+package com.ninos.apigatewaymicroservice3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayMicroservice3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayMicroservice3Application.class, args);
+	}
+
+}
