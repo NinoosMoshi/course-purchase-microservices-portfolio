469ed5184cf76aa3f86622670340e57123a3e9b2
NinoosMoshi <nalkaton@gmail.com>
2021-10-29 17:52:43 -0400

    Spring User class Closes #15

---
+++ backEnd/api-gateway-microservice3/src/main/java/com/ninos/apigatewaymicroservice3/model/Role.java
@@ -0,0 +1,6 @@
+package com.ninos.apigatewaymicroservice3.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ backEnd/api-gateway-microservice3/src/main/java/com/ninos/apigatewaymicroservice3/model/User.java
@@ -0,0 +1,37 @@
+package com.ninos.apigatewaymicroservice3.model;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", unique = true, nullable = false, length = 100)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @CreationTimestamp
+    @Column(name = "create_time", nullable = false)
+    private LocalDateTime createTime;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+
+
+}
