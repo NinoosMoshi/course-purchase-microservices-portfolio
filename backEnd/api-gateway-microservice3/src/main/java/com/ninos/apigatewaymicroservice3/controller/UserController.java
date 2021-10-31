0fa82dd78b8edeac7a343443567a9e2655b08697
NinoosMoshi <nalkaton@gmail.com>
2021-10-31 07:37:15 -0400

    Spring UserController Closes #26

---
+++ backEnd/api-gateway-microservice3/src/main/java/com/ninos/apigatewaymicroservice3/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.ninos.apigatewaymicroservice3.controller;
+
+import com.ninos.apigatewaymicroservice3.model.Role;
+import com.ninos.apigatewaymicroservice3.security.UserPrincipal;
+import com.ninos.apigatewaymicroservice3.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/user") //pre-path
+public class UserController
+{
+    @Autowired
+    private UserService userService;
+
+    @PutMapping("/change/{role}")   //http://localhost:5555/api/user/change/{role}
+    public ResponseEntity<?> changeRole(@AuthenticationPrincipal UserPrincipal userPrincipal, @PathVariable Role role) {
+        userService.changeRole(role, userPrincipal.getUsername());
+        return ResponseEntity.ok(true);
+    }
+}
