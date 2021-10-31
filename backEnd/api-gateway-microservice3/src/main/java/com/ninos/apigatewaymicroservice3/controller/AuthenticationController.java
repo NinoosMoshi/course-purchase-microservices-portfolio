e490847847ad9a7e6d3c18f353f9a9755a922b80
NinoosMoshi <nalkaton@gmail.com>
2021-10-30 20:25:28 -0400

    Spring AuthenticationController Closes #24

---
+++ backEnd/api-gateway-microservice3/src/main/java/com/ninos/apigatewaymicroservice3/controller/AuthenticationController.java
@@ -0,0 +1,39 @@
+package com.ninos.apigatewaymicroservice3.controller;
+import com.ninos.apigatewaymicroservice3.model.User;
+import com.ninos.apigatewaymicroservice3.service.AuthenticationService;
+import com.ninos.apigatewaymicroservice3.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("api/authentication")
+public class AuthenticationController {
+
+    private AuthenticationService authenticationService;
+    private UserService userService;
+
+    @Autowired
+    public AuthenticationController(AuthenticationService authenticationService, UserService userService) {
+        this.authenticationService = authenticationService;
+        this.userService = userService;
+    }
+
+    @PostMapping("/sign-up")//api/authentication/sign-up
+    public ResponseEntity<?> signUp(@RequestBody User user) {
+        if (userService.findByUsername(user.getUsername()).isPresent()) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+        return new ResponseEntity<>(userService.saveUser(user), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/sign-in")//api/authentication/sign-in
+    public ResponseEntity<?> signIn(@RequestBody User user) {
+        return new ResponseEntity<>(authenticationService.signInAndReturnJWT(user), HttpStatus.OK);
+    }
+}
