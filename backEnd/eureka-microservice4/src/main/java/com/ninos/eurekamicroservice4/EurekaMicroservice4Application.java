6704b4153ee7a75f38e5632a94cbbb0bc08de80f
NinoosMoshi <nalkaton@gmail.com>
2021-11-03 09:43:48 -0400

    Spring application.properties Closes #32

--- backEnd/eureka-microservice4/src/main/java/com/ninos/eurekamicroservice4/EurekaMicroservice4Application.java
+++ backEnd/eureka-microservice4/src/main/java/com/ninos/eurekamicroservice4/EurekaMicroservice4Application.java
@@ -2,7 +2,9 @@ package com.ninos.eurekamicroservice4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaMicroservice4Application {
 
