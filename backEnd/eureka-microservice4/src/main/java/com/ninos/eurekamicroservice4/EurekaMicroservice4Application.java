5c869a82a29ec6232f6b62b10b8001bc1b6a1ed9
NinoosMoshi <nalkaton@gmail.com>
2021-11-03 09:35:44 -0400

    Spring eureka-server microservice Closes #31

---
+++ backEnd/eureka-microservice4/src/main/java/com/ninos/eurekamicroservice4/EurekaMicroservice4Application.java
@@ -0,0 +1,13 @@
+package com.ninos.eurekamicroservice4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaMicroservice4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaMicroservice4Application.class, args);
+	}
+
+}
---
+++ backEnd/eureka-microservice4/src/test/java/com/ninos/eurekamicroservice4/EurekaMicroservice4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ninos.eurekamicroservice4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaMicroservice4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
