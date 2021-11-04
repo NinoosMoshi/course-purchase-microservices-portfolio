bbe9a8e4d6799d3ad809c1dfa93ed3ac5dbf26ba
NinoosMoshi <nalkaton@gmail.com>
2021-11-04 12:36:17 -0400

    Angulare routerPath Closes #40

--- frontEnd/course-purchase-app/src/app/app-routing.module.ts
+++ frontEnd/course-purchase-app/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { UnauthorizedComponent } from './components/unauthorized/unauthorized.component';
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: 'home', pathMatch: 'full'},
+  {path:'home', component:HomeComponent},
+  {path:'login', component:LoginComponent},
+  {path:'register', component:RegisterComponent},
+  {path:'profile', component:ProfileComponent},
+  {path:'admin', component:AdminComponent},
+  {path:'404', component:NotFoundComponent},
+  {path:'401', component:UnauthorizedComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
